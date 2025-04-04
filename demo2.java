55169d1f3a1fd7c13e03c8bbc0269d5a902cefb1
varma <kssjvarms@gmail.com>
2025-04-04 12:30:39 +0530

    second commit to develop branch

--- demo2.java
+++ demo2.java
@@ -1,5 +1,6 @@
 public class B{
     public static void main(String[] args){
-        System.out.println("class b")
+        System.out.println("class b");
+        System.out.println("b");
     }
 }
